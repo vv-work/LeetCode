2e739a1fc078a95f2b5cef5c6af2f542cc727f54
vv-work <81935179+vv-work@users.noreply.github.com>
2022-07-12 23:39:11 +0300

    Time: 115 ms (47.99%), Space: 15.3 MB (14.30%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,17 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #O(n) vs O(n^2)
-        e = set()
         dic ={};
         # Add all number
         for i in range(len(nums)):
             v = nums[i]
-            e.add(v)
             dic[v] = i;
         for i in range(len(nums)):
             v = nums[i]
             g = target - v;                      
-            if g in e: 
+            if g in dic: 
                 if i!=dic[g]:
                     l = i                                      
                     r = dic[g]                                                 
