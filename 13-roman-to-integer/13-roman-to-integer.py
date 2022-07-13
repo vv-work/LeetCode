6672d631ee49306878b3f38840d7e7ba596fbd7b
vv-work <81935179+vv-work@users.noreply.github.com>
2022-07-14 01:13:00 +0300

    Time: 71 ms (58.43%), Space: 13.7 MB (98.47%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,29 +1,29 @@
 class Solution:
     
 
-    def charToInt(self,c:int) -> int:
-        if c==ord('I'):
+    def charToInt(self,c:str) -> int:
+        if (c=='I'):
             return 1
-        if c==ord('V'):
+        if (c=='V'):
             return 5
-        if c==ord('X'):
+        if (c=='X'):
             return 10
-        if c==ord('L'):
+        if (c=='L'):
             return 50
-        if c==ord('C'):
+        if (c=='C'):
             return 100
-        if c==ord('D'):
+        if (c=='D'):
             return 500
-        if c==ord('M'):
+        if (c=='M'):
             return 1000
         return -1    
     
     def romanToInt(self, s: str) -> int:
         r=0
         for i in range(len(s)):
-            val=self.charToInt(ord(s[i]))
+            val=self.charToInt(s[i])
             if(i+1<len(s)):
-                nVal = self.charToInt(ord(s[i+1]))
+                nVal = self.charToInt(s[i+1])
                 if(val<nVal):
                     val=val*-1
             r= r+val
