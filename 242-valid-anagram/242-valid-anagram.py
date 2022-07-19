a12bb01f4b4f7cc8dbb639513ddc26195a5950a8
vv-work <81935179+vv-work@users.noreply.github.com>
2022-07-19 03:25:58 +0300

    Time: 51 ms (90.55%), Space: 14.5 MB (34.60%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dicS = dict()
+        for c in s:
+            if(c in dicS):
+                dicS[c] = dicS[c]+1
+            else:
+                dicS[c] = 1
+
+        for c in t:
+            if c in dicS:
+                dicS[c] = dicS[c]-1
+                if dicS[c]<=0 :
+                    del dicS[c]
+            else:
+                return False
+        if(len(dicS)>0):
+            return False
+
+        return True
